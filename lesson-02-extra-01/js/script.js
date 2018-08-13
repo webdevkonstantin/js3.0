5212f167f8ca05e3c2d5ba7aca23ef4621790cfd
Konstantin <skkonstantinovich@gmail.com>
2018-08-14 00:15:04 +0300

    Урок №2. Усложненное задание №1

--- lesson-02-extra-01/js/script.js
+++ lesson-02-extra-01/js/script.js
@@ -1,1 +1,16 @@
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let parent = document.getElementById('week');
+let date = new Date(); // текущая дата
+// let currentDay = week[date.getDay()]; // текущий день недели
+
+for (let i = 0; i < week.length; i++) {
+	let child = document.createElement('li');
+	// if ( week[i] === 'Суббота' || week[i] === 'Воскресенье' ) {
+	if ( i === 5 || i === 6 ) {
+		child.innerHTML = '<strong>' + week[i] + '</strong>';
+	} else if (i === date.getDay()-1) {
+		child.innerHTML = '<em>' + week[i] + '</em>';
+	}
+	else child.innerHTML = week[i];
+	parent.appendChild(child);
+}
