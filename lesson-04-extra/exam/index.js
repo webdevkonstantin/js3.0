7e11afc45d700cba9c8372b9ffabac6439195c23
git <e.tachalov@eos.local>
2018-08-16 16:55:34 +0300

    Урок №4. Дополнительное задание.

---
+++ lesson-04-extra/exam/index.js
@@ -0,0 +1,67 @@
+function getFriendlyNumbers(start, end) {
+    let friendlyArr = [];
+    if (start > 0 && end > 0 && start <= end && typeof start === 'number' && typeof end === 'number' && start !== null && end !== null && (start % 1 === 0) && (end % 1 === 0)) {
+        for ( let i = start; i <= end; i++ ) {
+            let num1 = getDivisorsSum(i);
+            let num2 = getDivisorsSum(num1);
+
+            if ( num1 !== num2 && num1 < num2 && isFriendly(num1, num2) ) {
+                let nums = [];
+                nums.push(num1,num2);
+
+                if ( !inArray(friendlyArr, nums) ) {
+                    friendlyArr.push(nums);
+                }
+            }
+        }
+    } else return false;
+
+    return friendlyArr;
+}
+
+// Сравниваем 2 массива путем преобразования их в строку
+function inArray(arr1, arr2) {
+    let inArray = false;
+    for ( let i = 0; i < arr1.length; i++ ) {
+        if ( JSON.stringify(arr1[i]) === JSON.stringify(arr2)  ) inArray = true;
+    }
+
+    return inArray
+}
+
+// Проверяем равна ли сумма делителей первого числа второму числу
+// и наобарот равна ли сумма делителей второго числа первому числу
+function isFriendly(num1, num2) {
+    return getDivisorsSum(num1) === num2 && getDivisorsSum(num2) === num1
+}
+
+// Получаем сумму делителей числа
+function getDivisorsSum(num) {
+    return getSum(getDivisors(num))
+}
+
+// Получаем массив делителей числа num
+function getDivisors(num) {
+    let arr = [];
+    for (let i = 1; i < num; i++) {
+        if ( num % i === 0 ) arr.push(i);
+    }
+
+    return arr;
+}
+
+// Получаем сумму чисел из массива
+function getSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum+=arr[i];
+    }
+
+    return sum;
+}
+
+module.exports = {
+    firstName: 'Konstantin',
+    secondName: 'Stasenko',
+    task: getFriendlyNumbers
+};
