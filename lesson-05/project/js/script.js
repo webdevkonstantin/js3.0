3e9b14ff2d75293f96fc5442e1e40a68d583ed80
git <e.tachalov@eos.local>
2018-08-20 17:21:34 +0300

    Урок №5. Второе обязательное задание.

---
+++ lesson-05/project/js/script.js
@@ -0,0 +1,29 @@
+let openBtn         = document.getElementById('open-btn'),
+    nameValue       = document.querySelector('.name-value'),
+    budgetValue     = document.querySelector('.budget-value'),
+    goodsValue      = document.querySelector('.goods-value'),
+    itemsValue      = document.querySelector('.items-value'),
+    employersValue  = document.querySelector('.employers-value'),
+    discountValue   = document.querySelector('.discount-value'),
+    isopenValue     = document.querySelector('.isopen-value'),
+    goodsItems      = document.querySelectorAll('.goods-item'),
+    buttons         = document.getElementsByTagName('button'), // Получает все 4 кнопки на странице, а надо 3, хм...
+    chooseItem      = document.querySelector('.choose-item'),
+    timeValue       = document.querySelector('.time-value'),
+    countBudgetValue= document.querySelector('.count-budget-value'),
+    employers       = document.querySelectorAll('.hire-employers-item');
+
+console.log('openBtn: ',openBtn);
+console.log('nameValue: ',nameValue);
+console.log('budgetValue: ',budgetValue);
+console.log('goodsValue: ',goodsValue);
+console.log('itemsValue: ',itemsValue);
+console.log('employersValue: ',employersValue);
+console.log('discountValue: ',discountValue);
+console.log('isopenValue: ',isopenValue);
+console.log('goodsItems: ',goodsItems);
+console.log('buttons: ',buttons);
+console.log('chooseItem: ',chooseItem);
+console.log('timeValue: ',timeValue);
+console.log('countBudgetValue: ',countBudgetValue);
+console.log('employers: ',employers);
