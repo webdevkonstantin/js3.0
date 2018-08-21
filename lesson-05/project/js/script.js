c1aa28c6fee010bc569daf86806253804c5039e3
git <e.tachalov@eos.local>
2018-08-21 11:51:08 +0300

    Урок №5. Второе обязательное задание.

--- lesson-05/project/js/script.js
+++ lesson-05/project/js/script.js
@@ -1,13 +1,17 @@
 let openBtn         = document.getElementById('open-btn'),
-    nameValue       = document.querySelector('.name-value'),
-    budgetValue     = document.querySelector('.budget-value'),
-    goodsValue      = document.querySelector('.goods-value'),
-    itemsValue      = document.querySelector('.items-value'),
-    employersValue  = document.querySelector('.employers-value'),
-    discountValue   = document.querySelector('.discount-value'),
-    isopenValue     = document.querySelector('.isopen-value'),
-    goodsItems      = document.querySelectorAll('.goods-item'),
-    buttons         = document.getElementsByTagName('button'), // Получает все 4 кнопки на странице, а надо 3, хм...
+    nameValue       = document.getElementsByClassName('name-value')[0],
+    budgetValue     = document.getElementsByClassName('budget-value')[0],
+    goodsValue      = document.getElementsByClassName('goods-value')[0],
+    itemsValue      = document.getElementsByClassName('items-value')[0],
+    employersValue  = document.getElementsByClassName('employers-value')[0],
+    discountValue   = document.getElementsByClassName('discount-value')[0],
+    isopenValue     = document.getElementsByClassName('isopen-value')[0],
+
+    goodsItems      = document.getElementsByClassName('goods-item'),
+    goodsBtn        = document.getElementsByTagName('button')[1],
+    budgetBtn       = document.getElementsByTagName('button')[2],
+    employersBtn    = document.getElementsByTagName('button')[3],
+
     chooseItem      = document.querySelector('.choose-item'),
     timeValue       = document.querySelector('.time-value'),
     countBudgetValue= document.querySelector('.count-budget-value'),
@@ -22,7 +26,9 @@ console.log('employersValue: ',employersValue);
 console.log('discountValue: ',discountValue);
 console.log('isopenValue: ',isopenValue);
 console.log('goodsItems: ',goodsItems);
-console.log('buttons: ',buttons);
+console.log('goodsBtn: ',goodsBtn);
+console.log('budgetBtn: ',budgetBtn);
+console.log('employersBtn: ',employersBtn);
 console.log('chooseItem: ',chooseItem);
 console.log('timeValue: ',timeValue);
 console.log('countBudgetValue: ',countBudgetValue);
