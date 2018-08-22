ca41ce2b616b02363e548ec7c5d40e0eaf6957f6
git <e.tachalov@eos.local>
2018-08-22 16:12:45 +0300

    Урок №6. Обязательное задание.

--- lesson-06/js/script.js
+++ lesson-06/js/script.js
@@ -1,34 +1,87 @@
 let openBtn         = document.getElementById('open-btn'),
     nameValue       = document.getElementsByClassName('name-value')[0],
     budgetValue     = document.getElementsByClassName('budget-value')[0],
+    budgetField     = document.getElementById('budget'),
     goodsValue      = document.getElementsByClassName('goods-value')[0],
     itemsValue      = document.getElementsByClassName('items-value')[0],
     employersValue  = document.getElementsByClassName('employers-value')[0],
     discountValue   = document.getElementsByClassName('discount-value')[0],
     isopenValue     = document.getElementsByClassName('isopen-value')[0],
 
-    goodsItem      = document.getElementsByClassName('goods-item'),
+    goodsItem       = document.getElementsByClassName('goods-item'),
     goodsBtn        = document.getElementsByTagName('button')[1],
     budgetBtn       = document.getElementsByTagName('button')[2],
     employersBtn    = document.getElementsByTagName('button')[3],
+    discountBtn     = document.getElementsByTagName('button')[4],
 
     chooseItem      = document.querySelector('.choose-item'),
     timeValue       = document.querySelector('.time-value'),
     countBudgetValue= document.querySelector('.count-budget-value'),
-    employers       = document.querySelectorAll('.hire-employers-item');
+    employers       = document.querySelectorAll('.hire-employers-item'),
+    priceValue      = document.querySelector('.price-item-value');
 
- let budget,
-     price;
+ let budget;
 
-openBtn.addEventListener('click', () => {
-    while (isNaN(budget) || budget === '' || budget == null) {
-        budget = prompt("Ваш бюджет на месяц?", "");
-    }
+ // Запрет ручного ввода в input “Расчет дневного бюджета”
+budgetField.disabled = true;
 
-    budgetValue.textContent = budget + ' рублей';
-    nameValue.textContent = prompt("Название вашего магазина?", "Киберкот").toUpperCase();
-});
+// Делаем все кнопки и ввод времени по умолчанию недоступными
+goodsBtn.disabled = true;
+budgetBtn.disabled = true;
+goodsDisabled(true);
+chooseItem.disabled = true;
+timeValue.disabled = true;
+employersDisabled(true);
+employersBtn.disabled = true;
+discountBtn.disabled = true;
 
+// нажатие кнопки "Открыть магазин"
+function open() {
+    openBtn.addEventListener('click', () => {
+        while (isNaN(budget) || budget === '' || budget == null) {
+            budget = prompt("Ваш бюджет на месяц?", "70000");
+        }
+
+        budgetValue.textContent = budget + ' рублей';
+        nameValue.textContent = prompt("Название вашего магазина?", "Пиченька").toUpperCase();
+        mainList.shopName = nameValue.value;
+        mainList.open = true;
+        chooseItem.disabled = false;
+        budgetBtn.disabled = false;
+        timeValue.disabled = false;
+        timeValue.value = formatTime(new Date());
+        changeTime();
+        goodsDisabled(false);
+        employersDisabled(false);
+        discountBtn.disabled = false;
+        console.log(timeValue.value);
+    });
+
+}
+open();
+
+// Проверяем ввод значений в поля ввода категории товаров
+// и проверяем открыт ли магазин
+for (let i = 0; i < goodsItem.length; i++) {
+    goodsItem[i].addEventListener('input', function () {
+        if (mainList.open) {
+            if (goodsItem[i].value.trim()) goodsBtn.disabled = false;
+            else {
+                let allgoods = '';
+                for (let i = 0; i < goodsItem.length; i++) {
+                    allgoods += goodsItem[i].value.trim();
+                }
+                console.log('allgoods: ', allgoods);
+                if (allgoods == '') goodsBtn.disabled = true;
+            }
+        } else {
+            goodsItem[i].value = '';
+            alert('Магазин закрыт!')
+        }
+    });
+}
+
+// нажатие кнопки "Утвердить"
 goodsBtn.addEventListener('click', () => {
     for (let i = 0; i < goodsItem.length; i++) {
         let answer = goodsItem[i].value;
@@ -42,9 +95,9 @@ goodsBtn.addEventListener('click', () => {
             console.log("Тип товара не был добавлен! Введите еще раз!");
         }
     }
-    return mainList.shopGoods;
 });
 
+// ввод продуктов через запятую
 // вместо change используем input - отображает изменения сразу при изменении
 chooseItem.addEventListener('input', () => {
     let items = chooseItem.value;
@@ -58,67 +111,124 @@ chooseItem.addEventListener('input', () => {
 
 });
 
-timeValue.addEventListener('change', function () {
-    let time = timeValue.value;
-    console.log('time: ', time);
-    console.log('typeof time: ', typeof time);
+// Ввод в поле "Сколько сейчас времени?"
+timeValue.addEventListener('input', () => {
+    changeTime();
+});
 
-    if ( time < 0 ) {
-        mainList.open = false;
-        console.log("Такого не может быть!");
-    } else if ( time > 8 && time < 20 ) {
-        mainList.open = true;
-        console.log("Время работать!");
-    } else if ( time < 24 ) {
-        mainList.open = false;
-        console.log("Уже слишком поздно!");
+// нажатие кнопки "Рассчитать"
+budgetBtn.addEventListener('click', () => {
+    if (budget) countBudgetValue.value = (budget / 30).toFixed(2);
+});
+
+// нажатие кнопки "Нанять"
+employersBtn.addEventListener('click', () => {
+    for ( let i = 0; i < employers.length; i++) {
+        let employerName = employers[i].value;
+        mainList.employers[i] = employerName;
+
+        employersValue.textContent += mainList.employers[i] + ', ';
+    }
+    // employersValue.textContent = ''+mainList.employers;
+});
+
+// нажатие кнопки "Получить скидку"
+discountBtn.addEventListener('click', () => {
+    let discount = 0,
+        price = priceValue.value;
+
+    if (price > 0 && price !== '') {
+        mainList.discount = true;
+
+        if (price > 0 && price <= 2000) {
+            discount = 5;
+            price = price * 0.95;
+        } else if (price > 2000 && price <= 4000) {
+            discount = 10;
+            price = price * 0.9;
+        } else if (price > 4000 && price <= 6000) {
+            discount = 15;
+            price = price * 0.85;
+        } else if (price > 6000 && price <= 8000) {
+            discount = 20;
+            price = price * 0.8;
+        } else if (price > 8000 && price <= 10000) {
+            discount = 25;
+            price = price * 0.75;
+        } else if (price > 10000) {
+            discount = 30;
+            price = price * 0.7;
+        }
     } else {
-        mainList.open = false;
-        console.log("В сутках только 24 часа!");
+        mainList.discount = false;
+    }
+
+    if (mainList.discount) {
+        discountValue.style.color = 'white';
+        discountValue.style.backgroundColor = 'green';
+        discountValue.innerHTML = 'Скидка: ' + discount + '%.<br>Стоимость со скидкой: ' + price.toFixed(2);
+    } else {
+        discountValue.style.backgroundColor = 'red';
+        discountValue.innerHTML = '';
     }
 });
 
 let mainList = {
-    budget : budgetValue,
+    budget : budget,
     shopName : nameValue,
-    // budget : 0,
-    // shopName : '',
     shopGoods : [],
     employers : {},
     open : false,
-    discount : true,
-    shopItems: [],
-    // chooseGoods : function () {},
-    // workTime : function (time) {},
-    dayBudget : function () {
-        alert('Ваш бюджет на 1 день составит: ' + (mainList.budget/30).toFixed(2));
-    },
-    makeDiscount : function () {
-        if ( mainList.discount ) price = price*0.8;
-        console.log('Итоговая стоимость товара со скидкой: ', price);
-    },
-    hireEmployers : function () {
-        for (let i=0; i<4; i++) {
-            let employer = prompt("Имя нового сотрудника?","");
-            if ( (typeof employer) === 'string' && (typeof employer) !== null && employer !== '' && employer.length < 50) {
-                console.log("Новый сотрудник!");
-                mainList.employers[i] = employer;
-            } else {
-                console.log("Не указано имя сотрудника!");
-            }
-        }
-        return mainList.employers;
-    },
-    // chooseShopItem: function () {},
-    showMainList : function () {
-        console.log('Наш магазин включает в себя: ');
-        for ( let key in mainList ) {
-            console.log('Свойство ' + key + ' имеет значение ' + mainList[key]);
-        }
-    }
+    discount : false,
+    shopItems: []
 };
 
-console.log('mainList: ', mainList);
-// mainList.addEventListener('change', function () {
-//
-// });
+function changeTime() {
+    let time = timeValue.value,
+        timeArr = time.split(':');
+
+    console.log('time: ', time);
+
+    if ( +timeArr[0] >= 8 && +timeArr[0] < 20 ) {
+        mainList.open = true;
+        console.log("Время работать!");
+    } else if ( +timeArr[0] >= 20 && +timeArr[0] <= 23) {
+        mainList.open = false;
+        console.log("Уже слишком поздно!");
+    } else {
+        mainList.open = false;
+        console.log("Магазин еще не открылся!");
+    }
+
+    if (mainList.open === true) {
+        isopenValue.style.backgroundColor = 'green';
+        isopenValue.style.color = 'white';
+        isopenValue.innerHTML = 'Магазин открыт'
+    } else {
+        isopenValue.style.backgroundColor = 'red';
+        isopenValue.style.color = 'white';
+        isopenValue.innerHTML = 'Магазин закрыт'
+    }
+}
+
+function formatTime(date) {
+    let hh = date.getHours(),
+        mm = date.getMinutes();
+
+    if (hh < 10) hh = '0' + hh;
+    if (mm < 10) mm = '0' + mm;
+
+    return hh + ':' + mm;
+}
+
+function goodsDisabled(disabled) {
+    for (let i = 0; i < goodsItem.length; i++) {
+        goodsItem[i].disabled = disabled;
+    }
+}
+
+function employersDisabled(disabled) {
+    for (let i = 0; i < employers.length; i++) {
+        employers[i].disabled = disabled;
+    }
+}
