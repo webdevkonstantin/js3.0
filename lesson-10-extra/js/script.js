241035661203882f7360b5b86f6abd73a48cace6
Konstantin <skkonstantinovich@gmail.com>
2018-08-29 22:22:05 +0300

    Урок №10. Дополнительное задание

---
+++ lesson-10-extra/js/script.js
@@ -0,0 +1,12 @@
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(10, 15);
