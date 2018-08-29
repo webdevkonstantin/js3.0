19f9dfb13139dc8123fa8c78f58353bacc6047f8
Konstantin <skkonstantinovich@gmail.com>
2018-08-29 23:06:32 +0300

    Урок №11. Теория.

--- lesson-11-theory/js/script.js
+++ lesson-11-theory/js/script.js
@@ -1,8 +1,16 @@
 // 1. Создаем объект запроса
 let request = new XMLHttpRequest();
 
+// 2. Настройка запроса
 // request.open(method);
-request.open("GET", 'server.php');
+request.open("GET", 'server.php', true, login, password);
+// 3. Отправка запроса
+request.send();
+
+request.status // 200, 404
+request.statusText
+request.responseText
+request.readyState
 
 // let options = {
 //   width: 1366,
